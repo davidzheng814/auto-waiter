41de30735474b876de1afb95a1bdbb720ce87fbb
David Zheng <dzheng@purestorage.com>
2016-06-30 13:44:10 -0700

    Add layer to communicate with Waiter API.

---
+++ api.py
@@ -0,0 +1,34 @@
+import requests
+
+def login(username, password):
+    data = {
+        'username':username,
+        'password':password,
+        'response_type':'code',
+        'redirect_uri':'https%3A%2F%2Fwww.waiter.com%2Foauth%2Fcallback'
+    }
+    r = requests.post('https://www.waiter.com/api/v1/login', data=data)
+    res = r.json()
+
+    if 'access_token' in res:
+        access_token = res['access_token']
+        user_id = res['user_id']
+    else:
+        return False
+
+    r = requests.get('https://www.waiter.com/home/{token}'.format(token=access_token))
+    session_cookie = r.cookies
+
+    return session_cookie
+
+def add_item(session_cookie, item):
+    r = requests.post('https://www.waiter.com/api/v1/cart_items.json', cookies=session_cookie, data=item)
+
+session_cookie = login('dzheng@purestorage.com', 'testpassword')
+
+add_item(session_cookie, {
+    'cart_id':3174148,
+    'menu_item_id':1173595,
+    'menu_item_option_choice_ids':5862793,
+    'quantity':1
+})
