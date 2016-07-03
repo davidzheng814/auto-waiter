6eafd0e5c77411f1913c4218ec682b397f3f80ec
Jeb Bearer <jbearer@hmc.edu>
2016-07-02 18:33:45 -0700

    Remove celery stuff

--- run.py
+++ run.py
@@ -5,8 +5,6 @@
    Note: Run as root on production, and user locally.
 """
 from getpass import getuser
-from subprocess import call
-
 from app import APP
 from util import parse_args
 
@@ -17,7 +15,3 @@ if getuser() == "root":
     APP.run(debug=False, host="172.31.11.17", port=80)
 else:
     APP.run(debug=True)
-
-# Kick off the scheduler
-call(['celery', '-A', 'tasks', 'beat', '--detach'])
-call(['celery', '-A', 'tasks', 'worker', '--detach'])
